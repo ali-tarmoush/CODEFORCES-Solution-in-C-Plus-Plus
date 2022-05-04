49a156d4abb74f0bc98694a608a322010c522717
Ali Tarmoush <104841920+ali-tarmoush@users.noreply.github.com>
2022-05-04 15:41:57 +0300

    Add files via upload

---
+++ domino-piling.cpp
@@ -0,0 +1,16 @@
+/*
+ * Problem Link   : https://codeforces.com/contest/50/problem/A
+ * Problem Name   : Domino piling
+ * Language:      : C++
+ * Programmer     : Ali Tarmoush 
+ * Status         : Accepted
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+      int m,n ;
+      cin>>m>>n;
+      cout<<(m*n)/2;
+      return 0;
+}
