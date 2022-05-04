b4f52bb8051798ca676ff96bce21aa6f509d06a4
Ali Tarmoush <104841920+ali-tarmoush@users.noreply.github.com>
2022-05-04 16:35:28 +0300

    Add files via upload

---
+++ HQ9.cpp
@@ -0,0 +1,23 @@
+/*
+ * Problem Link   : https://codeforces.com/problemset/problem/133/A
+ * Problem Name   : HQ9+
+ * Language:      : C++
+ * Programmer     : Ali Tarmoush 
+ * Status         : Accepted
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	string st;
+	cin >> st;
+	bool p;
+	for (int i = 0; i < st.size(); i++)
+	if (st[i] == 'H' || st[i] == 'Q' || st[i] == '9' || st[i] == ' + '){
+		p = 1;
+		break;
+	}
+	else
+		p = 0;
+	(p == 1)?cout << "YES":cout << "NO";
+	return 0;
+}
