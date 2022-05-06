39621815b99dcb59b8355edc101edb5780465f2f
Ali Tarmoush <104841920+ali-tarmoush@users.noreply.github.com>
2022-05-06 23:58:45 +0300

    Add files via upload

---
+++ soldier-and-bananas.cpp
@@ -0,0 +1,22 @@
+/*
+ * Problem Link   : https://codeforces.com/problemset/problem/546/A
+ * Problem Name   : Soldier and Bananas
+ * Language:      : C++
+ * Programmer     : Ali Tarmoush 
+ * Status         : Accepted
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int k, n, w;
+	cin >> k >> n >> w;
+	int sum = 0;
+	for (int i = 1; i <=w; i++){
+		sum = sum + (k*i);
+	}
+	if (sum > n)
+		cout << sum - n;
+	else
+		cout << 0;
+	return 0;
+}
