05c1e479867f547a9effb938f402c94d2eb3e7d8
Ali Tarmoush <104841920+ali-tarmoush@users.noreply.github.com>
2022-07-09 23:43:04 +0300

    Create vanya-and-fence.cpp

---
+++ vanya-and-fence.cpp
@@ -0,0 +1,24 @@
+/*
+ * Problem Link   : https://codeforces.com/problemset/problem/677/A
+ * Problem Name   : Vanya and Fence
+ * Language:      : C++
+ * Programmer     : Ali Tarmoush 
+ * Status         : Accepted
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+	int n,h;
+	cin>>n>>h;
+	int a,sum=0;
+	for(int i=0;i<n;i++){
+		cin>>a;
+		if(a>h)
+		sum+=2;
+		else
+		sum++;
+	}
+	cout<<sum;
+    return 0 ;
+}
