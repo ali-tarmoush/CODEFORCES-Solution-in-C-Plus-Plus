9acef9cc1caa9db82e17207fee4f4bb2beaf2d4e
Ali Tarmoush <104841920+ali-tarmoush@users.noreply.github.com>
2022-07-09 23:49:18 +0300

    Create magnets.cpp

---
+++ magnets.cpp
@@ -0,0 +1,24 @@
+/*
+ * Problem Link   : https://codeforces.com/problemset/problem/344/A
+ * Problem Name   : Magnets
+ * Language:      : C++
+ * Programmer     : Ali Tarmoush 
+ * Status         : Accepted
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int n,t=1;
+	cin>>n;
+	string st1,st2;
+	cin>>st1;
+	for(int i=1;i<n;i++){
+		st2=st1;
+		cin>>st1;
+		if(st2!=st1)
+		t++;		
+	}
+	cout<<t;
+    return 0 ;
+}
