e31279ae806246148e9a0051e9b4d81707a8c98c
Ali Tarmoush <104841920+ali-tarmoush@users.noreply.github.com>
2022-07-09 23:40:47 +0300

    Create anton-and-polyhedrons.cpp

---
+++ anton-and-polyhedrons.cpp
@@ -0,0 +1,31 @@
+/*
+ * Problem Link   : https://codeforces.com/problemset/problem/785/A
+ * Problem Name   : Anton and Polyhedrons
+ * Language:      : C++
+ * Programmer     : Ali Tarmoush 
+ * Status         : Accepted
+ */
+ 
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+	int n;
+	cin >>n;
+	string st;
+	int sum=0;
+	for(int i=0;i<n;i++){
+		cin>>st;
+		if(st=="Tetrahedron")
+		sum+=4 ;
+		else if(st=="Cube")
+		sum+=6 ;
+		else if(st=="Octahedron")
+		sum+=8 ;
+		else if(st=="Dodecahedron")
+		sum+=12 ;
+		else if(st=="Icosahedron")
+		sum+=20;
+	}
+	cout<<sum;
+    return 0 ;
+}
