f5264a76146b38f9f10fd8e8467390b5bb788d87
Ali Tarmoush <104841920+ali-tarmoush@users.noreply.github.com>
2022-07-12 12:00:16 +0300

    Create beautiful-year.cpp

---
+++ beautiful-year.cpp
@@ -0,0 +1,25 @@
+/*
+ * Problem Link   : https://codeforces.com/problemset/problem/271/A
+ * Problem Name   : Beautiful Year
+ * Language:      : C++
+ * Programmer     : Ali Tarmoush 
+ * Status         : Accepted
+ */
+ 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int y,a=0,b=0,c=0,d=0;
+    cin >> y;
+    while (true){
+        y++;
+        a = y / 1000;
+        b = (y / 100 )% 10;
+        c = (y / 10 )% 10;
+        d = y % 10;
+        if (a != b && a != c && a != d && b != c && b != d && c != d)
+            break;
+    }
+    cout << y<<endl;
+    return 0;
+}
