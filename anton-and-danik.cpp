9e98d6b2a5d46e2ad8568aeef7fc50ed1c46267a
Ali Tarmoush <104841920+ali-tarmoush@users.noreply.github.com>
2022-07-12 11:40:49 +0300

    Create anton-and-danik.cpp

---
+++ anton-and-danik.cpp
@@ -0,0 +1,29 @@
+/*
+ * Problem Link   : https://codeforces.com/problemset/problem/734/A
+ * Problem Name   : Anton and Danik
+ * Language:      : C++
+ * Programmer     : Ali Tarmoush 
+ * Status         : Accepted
+ */
+ 
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int n,a=0,d=0;
+	cin>>n;
+	string st;
+	cin>>st;
+	for(int i=0;i<n;i++){
+		if(st[i]=='A')
+		a++;
+		else
+		d++;
+	}
+	if(a>d)
+	cout<<"Anton"<<endl;
+	else if(d>a)
+	cout<<"Danik"<<endl;
+	else
+	cout<<"Friendship"<<endl;
+	return 0;
+}
