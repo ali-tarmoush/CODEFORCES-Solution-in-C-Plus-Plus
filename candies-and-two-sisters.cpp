e97b8a031f79fc3ba665a17c329fbc413c6fb84c
Ali Tarmoush <104841920+ali-tarmoush@users.noreply.github.com>
2022-07-14 19:35:38 +0300

    Create candies-and-two-sisters.cpp

---
+++ candies-and-two-sisters.cpp
@@ -0,0 +1,26 @@
+/*
+ * Problem Link   : https://codeforces.com/problemset/problem/1335/A
+ * Problem Name   : Candies and Two Sisters
+ * Language:      : C++
+ * Programmer     : Ali Tarmoush 
+ * Status         : Accepted
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+     int t;
+     cin>>t;
+     for(int i=0;i<t;i++){
+        int n,a=0;
+        cin>>n;
+        if(n < 2)
+			a=0;
+        else if(n % 2 == 0)
+			a=n/2-1;
+		else
+			a=n/2;
+        cout<<a<<endl;
+     }
+    return 0;
+}
