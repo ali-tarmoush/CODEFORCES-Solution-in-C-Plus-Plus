33640109c203c05be82730145dae2de992660af5
Ali Tarmoush <104841920+ali-tarmoush@users.noreply.github.com>
2022-07-18 21:36:18 +0300

    Update beautiful-year.cpp

--- beautiful-year.cpp
+++ beautiful-year.cpp
@@ -9,6 +9,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     int y,a=0,b=0,c=0,d=0;
     cin >> y;
     while (true){
