3e8a9157e3e19a8d236e13e186ed106dba8d257a
Ali Tarmoush <104841920+ali-tarmoush@users.noreply.github.com>
2022-07-18 21:35:36 +0300

    Update arrival-of-the-general.cpp

--- arrival-of-the-general.cpp
+++ arrival-of-the-general.cpp
@@ -9,6 +9,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     int n,a,max=103,min=0,x=0,y=0;
     cin >> n;
     for (int i=0;i<n;i++) {
