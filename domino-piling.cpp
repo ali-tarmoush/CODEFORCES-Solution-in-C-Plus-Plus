a371dbfe836cef087e353b9a02f4b3b46f4c772b
Ali Tarmoush <104841920+ali-tarmoush@users.noreply.github.com>
2022-07-18 21:42:26 +0300

    Update domino-piling.cpp

--- domino-piling.cpp
+++ domino-piling.cpp
@@ -8,8 +8,10 @@
  
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
+int main(){
+	ios::sync_with_stdio(0);
+      cin.tie(0);
+      cout.tie(0);
       int m,n ;
       cin>>m>>n;
       cout<<(m*n)/2;
