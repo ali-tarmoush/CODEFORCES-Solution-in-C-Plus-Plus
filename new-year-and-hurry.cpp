cc8866586bf61ff6799c05594f4a70335ac49a85
Ali Tarmoush <104841920+ali-tarmoush@users.noreply.github.com>
2022-07-25 20:40:44 +0300

    Create new-year-and-hurry.cpp

---
+++ new-year-and-hurry.cpp
@@ -0,0 +1,28 @@
+/*
+ * Problem Link   : https://codeforces.com/problemset/problem/750/A
+ * Problem Name   : New Year and Hurry
+ * Language:      : C++
+ * Programmer     : Ali Tarmoush 
+ * Status         : Accepted
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n,k,sum=0;
+    cin>>n>>k;
+    int ct=240-k;
+    for(int i=1;i<=n;i++){
+        int pt=5*i;
+        if(pt<=ct){
+            ct-=pt;
+            sum++;
+        }
+        else
+            break;
+    }
+    cout<<sum<<endl;
+}
